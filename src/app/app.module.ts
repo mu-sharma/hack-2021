c9a3fd25ce36f09befbc99d8b663c4f878dcae7b
mu-sharma <33678494+mu-sharma@users.noreply.github.com>
2021-08-02 16:47:56 +0530

    commit number 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,39 +3,31 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddUserComponent } from './add-user/add-user.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { ListUserComponent } from './list-user/list-user.component';
 import { LoginComponent } from './login/login.component';
 import {UserService} from './service/user.service';
-import {SharedService} from './service/shared.service';
 import {HttpClientModule} from "@angular/common/http";
+import { ParentComponentComponent } from './parent-child/parent-component/parent-component.component';
+import { ChildComponentComponent } from './parent-child/parent-component/child-component/child-component.component';
 import { LinxModuleModule } from './linx-dashboard-module/linx-module/linx-module.module';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { ViewUserComponent } from './view-user/view-user.component';
-import { PatientComponent } from './patient/patient.component';
-import { DoctorComponent } from './doctor/doctor.component';
-import { HighchartsChartModule } from 'highcharts-angular';
-import { PatientHistoryComponent } from './common-components/patient-history/patient-history.component';
-import { PatientProfileComponent } from './patient-profile/patient-profile.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { GlobalService } from './service/global.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
-
-
+import { AddPrescriptionComponent } from './add-prescription/add-prescription.component';
 @NgModule({
   declarations: [
     AppComponent,
+    AddUserComponent,
     EditUserComponent,
     ListUserComponent,
     LoginComponent,
+    ParentComponentComponent,
+    ChildComponentComponent,
     ViewUserComponent,
-    PatientComponent,
-    DoctorComponent,
-    PatientHistoryComponent,
-    PatientProfileComponent
+    AddPrescriptionComponent
   ],
   imports: [
     BrowserModule,
@@ -46,13 +38,10 @@ import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
     LinxModuleModule,
     AngularFontAwesomeModule,
     TooltipModule.forRoot(),
-    ModalModule.forRoot(),
-    HighchartsChartModule,
-    NgbModule,
-    BrowserAnimationsModule,
-    NgMultiSelectDropDownModule.forRoot()
+    ModalModule.forRoot()
+   // RouterModule.forRoot(routes);
   ],
-  providers: [UserService, GlobalService,SharedService],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
 import {first} from "rxjs/operators";
-import * as users from './../../assets/users.json';
-import * as roles from './../../assets/acl.json';
 //import {AuthenticationService} from "../service/auth.service";
 
 @Component({
@@ -16,8 +14,6 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   submitted: boolean = false;
   invalidLogin: boolean = false;
-  users: any = (users as any).default;
-  roles: any = (roles as any).default;
   constructor(private formBuilder: FormBuilder, private router: Router) { }
 
   onSubmit() {
@@ -25,32 +21,14 @@ export class LoginComponent implements OnInit {
     if (this.loginForm.invalid) {
       return;
     }
-    var target=users.users.find(temp=>temp.userid==this.loginForm.controls.email.value)
-    if(target && this.loginForm.controls.password.value == 'password') {
-      users.users.forEach(element => {
-        if(element.userid==this.loginForm.controls.email.value){
-          let roleId=element.role;
-          roles.roles.forEach(element => {
-            if(element.id==roleId){
-              if(element.name=='patient'){
-               this.router.navigate(['app-patient',target]);
-              }
-              else{
-                this.router.navigate(['app-doctor',target]);
-              }
-            }
-            
-          });
-        }
-      });
-      //  this.router.navigate(['list-user']);
+    if(this.loginForm.controls.email.value == 'mik.sharma01@gmail.com' && this.loginForm.controls.password.value == 'password') {
+        this.router.navigate(['list-user']);
     }else {
       this.invalidLogin = true;
     }
   }
 
   ngOnInit() {
-   
     this.loginForm = this.formBuilder.group({
       email: ['', Validators.required],
       password: ['', Validators.required]
